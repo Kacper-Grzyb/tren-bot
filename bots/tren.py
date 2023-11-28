f82d8b88c0e6db30118d6ae3b2a2b2d9ff58f313
KacperGrzyb2137 <kgrzyb2860@gmail.com>
2023-11-28 17:51:02 +0100

    random move making and shitty move detection

--- bots/tren.py
+++ bots/tren.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from bots.base_bot import BaseBot
 import random
 from game.colors import Colors
@@ -16,6 +17,105 @@ class TrenBot(BaseBot):
         self.uid = uid
         self.winLength = 3
         # E.g initialize extra object variables
+
+    def interrupt_win(self, startX, startY) -> (int, int):
+        (x, y) = (startX, startY)
+        nextID = self.uid+1
+        # now check for lines in each direction from the placed point to know if player is going to win
+        # this is definitely not the most optimal way of doing this shit but it works
+
+        # check vertically down
+        i = 1
+        while x+1>len(self.board):
+            if i==self.winLength-1:
+                return x+1, y
+            x+=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check vertically up
+        i = 1
+        x, y = startX, startY
+        while x-1>0:
+            if i == self.winLength-1:
+                return x-1, y
+            x-=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check horizontally right
+        i = 1
+        x, y = 0, 0
+        while y+1<len(self.board[x]):
+            if i == self.winLength-1:
+                return x, y+1
+            y+=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check horizontally left
+        i = 1
+        x, y = startX, startY
+        while y-1>0:
+            if i == self.winLength-1:
+                return x, y-1
+            y-=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check diagonally up and right
+        i=1
+        x, y = startX, startY
+        while x+1<len(self.board) and y-1>0:
+            if(i == self.winLength):
+                return x+1, y-1
+            x+=1
+            y-=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check diagonally up and left
+        i=1
+        x, y = startX, startY
+        while x-1>0 and y-1>0 and i<self.winLength:
+            if i == self.winLength-1:
+                return x-1, y-1
+            x-=1
+            y-=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check diagonally down and right
+        i=1
+        x, y = startX, startY
+        while x+1<len(self.board) and y+1<len(self.board[x]) and i<self.winLength:
+            if i == self.winLength-1:
+                return x+1, y+1
+            x+=1
+            y+=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1
+        
+        # check diagonally down and left
+        i=1
+        x, y = startX, startY
+        while x-1>0 and y+1<len(self.board[x]) and i<self.winLength:
+            if i == self.winLength-1:
+                return x-1, y+1
+            x-=1
+            y+=1
+            if(self.board[x][y]!=nextID):
+                break
+            i+=1  
+            
+        return NULL, NULL
 
     def init_board(self, cols: int, rows: int, win_length: int, obstacles: [(int, int)], time_given: int) -> None:
         """
@@ -45,108 +145,29 @@ class TrenBot(BaseBot):
         Returns:
         tuple: containing the bot move with the order (x, y)
         """
-                
-
-        x = -1
-        y = -1
+        (x, y) = NULL, NULL
+        i = 0
+        j = 0
+        while i < len(self.board):
+            while j < len(self.board[i]):
+                if(j==self.uid+1): 
+                    (x, y) = self.interrupt_win(i, j)
+                    i = len(self.board)
+                    break
+                j+=1
+            i+=1
+                    
+        if (x, y) != (NULL, NULL):
+            return x, y
+        else:
+            x = random.randrange(0, len(self.board))
+            y = random.randrange(0, len(self.board[x]))
+            while(self.board[x][y]!=BoardCell.CLEAR):
+                x = random.randrange(0, len(self.board))
+                y = random.randrange(0, len(self.board[x]))
+  
         #I just made it return 0, 0 so our bot doesn't get immedietaly discualified
-        return 0, 0
-    
-    def interrupt_win(self, time_left: int) -> None:
-        nextID = self.uid+1
-        # now check for lines in each direction from the placed point to know if player is going to win
-        # this is definitely not the most optimal way of doing this shit but it works
-        # check vertically down
-        i = 1
-        while x+1>len(self.board) and i<self.winLength:
-            x+=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check vertically up
-        i = 1
-        while x-1>0 and i<self.winLength:
-            x-=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check horizontally right
-        i = 1
-        while y+1<len(self.board[x]) and i<self.winLength:
-            y+=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check horizontally left
-        i = 1
-        while y-1>0 and i<self.winLength:
-            y-=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check diagonally up and right
-        i=1
-        while x+1<len(self.board) and y-1>0 and i<self.winLength:
-            x+=1
-            y-=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check diagonally up and left
-        i=1
-        while x-11>0 and y-1>0 and i<self.winLength:
-            x-=1
-            y-=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check diagonally down and right
-        i=1
-        while x+1<len(self.board) and y+1<len(self.board[x]) and i<self.winLength:
-            x+=1
-            y+=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass
-        
-        # check diagonally down and left
-        i=1
-        while x-1>0 and y+1<len(self.board[x]) and i<self.winLength:
-            x-=1
-            y+=1
-            if(self.board[x][y]!=nextID):
-                break
-            i+=1
-        if i == self.winLength-1:
-            # store somewhere that player bot_uid is about to win
-            pass        
+        return x, y 
 
                 
             
@@ -155,7 +176,7 @@ class TrenBot(BaseBot):
         """
         This method is called when a move is made by a player.
 
-        Parameters:
+        Parameters: 
         bot_uid: The Unique ID of the player making the move.
         move: A tuple representing the move coordinates (x, y).
         """
